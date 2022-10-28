faf094ea82b42b843226785a55523e3bee69783e
TatoMesa <seba_mesa@hotmail.com>
2022-10-28 19:49:54 -0300

    Inicio tp4

---
+++ ejercicio1.php
@@ -0,0 +1,20 @@
+<?php
+
+/*Escriba un programa con una función que tome como parámetro dos enteros, los sume y devuelva 
+como resultado la suma. El programa deberá pedir al usuario los dos números, luego llamar a la 
+función con los números leídos como argumentos y por último informar al usuario el valor de la 
+suma.*/
+
+function suma($a,$b){
+    return $a + $b;
+};
+
+
+$a = readline("Ingrese el primer sumando: "). PHP_EOL;
+$b = readline("Ingrese el segundo sumando: "). PHP_EOL;
+
+$x = suma($a,$b);
+
+echo ( "La suma de ". $a ." y " .$b ."es: ". $x);
+
+?>
